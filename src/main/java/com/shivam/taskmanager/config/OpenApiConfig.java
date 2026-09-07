df1430fcb4860b2bc9371ea4f8b0ccfc954acd77
SHIVAM KUMAR <shivamkumarhub@gmail.com>
2026-09-07 20:19:48 +0530

    feat: OpenApiConfig - Swagger with JWT bearer auth scheme

---
+++ src/main/java/com/shivam/taskmanager/config/OpenApiConfig.java
@@ -0,0 +1,34 @@
+package com.shivam.taskmanager.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Swagger/OpenAPI configuration with JWT bearer auth scheme.
+ */
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI taskManagerOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Task Manager API")
+                        .description("Spring Boot 3 REST API for task management with JWT authentication")
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")));
+    }
+}
